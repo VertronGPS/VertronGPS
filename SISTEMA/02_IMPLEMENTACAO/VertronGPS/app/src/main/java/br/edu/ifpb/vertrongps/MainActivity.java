b2e7105ecd6640f12aeb83ae45bd5c3354d480f6
Andre Luis <tr.andreluis@gmail.com>
2017-06-02 19:19:06 -0300

    Modificação do documento de visão e adição das imagens do aplicativo

--- SISTEMA/02_IMPLEMENTACAO/VertronGPS/app/src/main/java/br/edu/ifpb/vertrongps/MainActivity.java
+++ SISTEMA/02_IMPLEMENTACAO/VertronGPS/app/src/main/java/br/edu/ifpb/vertrongps/MainActivity.java
@@ -2,6 +2,7 @@ package br.edu.ifpb.vertrongps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
